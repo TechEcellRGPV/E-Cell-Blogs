7d224d5d72ff7d82cde411199e21bf7d30e42a4c
Bharat940 <bdangi450@gmail.com>
2025-09-27 23:09:19 +0530

    Fix login route case sensitivity and add Vercel configuration for rewrites

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <div className="main">
       <Header />
       <Routes>
-        <Route path="/Login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route
           path="/"
           element={
